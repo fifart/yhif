641bdcb9e30b3a986a6376de3976422cb4b6e3ab
obiuno <fifart@gmail.com>
2017-10-27 14:23:02 +0000

    security

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,5 +1,5 @@
 class TracksController < ApplicationController
-  http_basic_authenticate_with name: "nicolas", password: "dales"
+  
   before_action :set_track, only: [:show, :edit, :update, :destroy]
   before_action :set_album
 
