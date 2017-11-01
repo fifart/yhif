7a4237a43570696a358f24eb8cf90bc91fa75d18
obiuno <fifart@gmail.com>
2017-11-01 10:26:16 +0000

    minor addictions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,12 +89,12 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'https://yhif.herokuapp.com/' port: 80 }
+  config.action_mailer.default_url_options = { host: 'https://yhif.herokuapp.com/', port: 80 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
       port:                  587,
-      domain:               'mail.google.com',
+      domain:               Rails.application.secrets.GMAIL_DOMAIN,
       user_name:            Rails.application.secrets.GMAIL_USERNAME,
       password:             Rails.application.secrets.GMAIL_PASSWORD,
       authentication:       'login',
